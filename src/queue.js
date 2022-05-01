9d84dce3befce99466698cdf83d24684dd4718d2
Boffin-ux <fnamily@mail.ru>
2022-05-01 15:29:27 +0300

    feat: completed 2 examples

--- src/queue.js
+++ src/queue.js
@@ -1,4 +1,5 @@
 const { NotImplementedError } = require('../extensions/index.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 // const { ListNode } = require('../extensions/list-node.js');
 
@@ -13,21 +14,36 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
+
 class Queue {
 
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.head = null;
+    this.length = 0;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  getUnderlyingList() {
+    return this.head;
+  }
+
+  enqueue(value) {
+    if (this.length === 0) {
+      this.head = new ListNode(value);
+    } else {
+      let current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = new ListNode(value);
+    }
+    this.length++;
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let current = this.head;
+    this.head = current.next;
+    this.length--;
+    return current.value;
   }
 }
 
--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -22,9 +22,80 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+  console.debug('l: ', l);
+  class LinkedList {
+    constructor() {
+      this.head = null;
+      this.length = 0;
+    }
+    addtList(value) {
+      this.head = value;
+      const count = (object) => {
+        for (const [value, next] of Object.entries(object)) {
+          if ((typeof next === "object" || typeof next === "number") && next !== null) {
+            if (typeof next === "number") {
+              this.length++;
+            }
+            count(next);
+          } else {
+            return;
+          }
+        }
+      };
+      count(this.head);
+    }
+    removeFromPosition(position) {
+      if (position < 0 || position > this.length) {
+        return 'Incorrect value of position';
+      }
+      let current = this.head;
+      if (position === 0) {
+        this.head = current.next;
+      } else {
+        let prev = null;
+        let index = 0;
+
+        while (index < position) {
+          prev = current;
+          current = current.next;
+          index++;
+        }
+        prev.next = current.next;
+      }
+      this.length--;
+      return current.value;
+    }
+    removeElementByValue(value) {
+      return this.removeFromPosition(this.indexOf(value));
+    }
+    indexOf(value) {
+      let current = this.head;
+      let index = 0;
+      while (current) {
+        if (current.value === value) {
+          return index;
+        }
+        current = current.next;
+        index++;
+      }
+      return -1;
+    }
+    getList() {
+      return this.head;
+    }
+  }
+  const linkedList = new LinkedList();
+  linkedList.addtList(l);
+  const remove = () => {
+    if (linkedList.removeElementByValue(k)) {
+      linkedList.removeElementByValue(k);
+    } else {
+      remove();
+    }
+  };
+  remove();
+  return linkedList.getList();
 }
 
 module.exports = {
